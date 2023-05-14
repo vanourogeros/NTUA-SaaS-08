465b378b876b55900bca8d4e7676d0caabc48428
Ioannis Protogeros <el19008@mail.ntua.gr>
2023-05-14 15:28:15 +0300

    Created docker-compose and file upload API
    App now runs on HTTP port 80 behind an nginx server to allow communication between services

--- microservice-parser/index.js
+++ microservice-parser/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const multer  = require('multer');
+const fs = require('fs');
 const upload = multer({ dest: 'uploads/' }); // uploads will be stored in an "uploads" directory in your project
 
 const app = express();
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
-    // req.file is the uploaded file object
-    console.log(req.file);
-    res.json({ status: 'OK' });
+    // req.file is the 'file' object
+    // req.file.path is path to the uploaded file
+
+    fs.readFile(req.file.path, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error reading file:', err);
+            return res.status(500).send('Error reading file');
+        }
+
+        console.log('File contents:', data);
+        res.json({ status: 'OK' });
+    });
 });
 
 app.listen(3001, () => {
