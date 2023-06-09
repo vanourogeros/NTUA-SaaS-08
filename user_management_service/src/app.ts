2e33171c87191a48c5c00355488f0bc96d29f3c3
Georgios Alexios Kapetanakis <el19062@mail.ntua.gr>
2023-06-09 23:27:28 +0300

    try/catch in app.ts

--- user_management_service/src/app.ts
+++ user_management_service/src/app.ts
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import { connectToDB } from "./lib/dbUtils.js";
@@ -6,64 +5,65 @@ import { verifyEnv } from "./lib/envUtils.js";
 import { getUserId } from "./middleware/auth.js";
 import userRouter from "./routes/user.js";
 
-// http response codes
-const codes = {
-    OK: 200,
-    CREATED: 201,
-    UNAUTHORIZED: 401,
-    INTERNAL_SERVER_ERROR: 500,
-};
+try {
+    // load environment variables
+    if (process.env.NODE_ENV !== "production") {
+        const dotenv = await import("dotenv");
+        dotenv.config({ path: ".env" });
+        console.log("Loaded the .env variables");
+    }
 
-// load environment variables
-if (process.env.NODE_ENV !== "production") {
-    dotenv.config({ path: ".env" });
-    console.log("Loaded the .env variables");
-}
-
-// verify all required environment variables exist
-const env = verifyEnv(
-    {
+    // verify all required environment variables exist
+    const env = verifyEnv({
         HTTP_HOST: process.env.HTTP_HOST,
         HTTP_PORT: process.env.HTTP_PORT,
         MONGO_HOST: process.env.MONGO_HOST,
         MONGO_PORT: process.env.MONGO_PORT,
         MONGO_NAME: process.env.MONGO_NAME,
-    },
-    (key) => {
-        console.error(`Environment variable '${key}' is missing`);
-        process.exit(-1);
-    }
-);
+    });
 
-// connect to the database and retry up to 3 times if it fails
-await connectToDB(
-    `mongodb://${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_NAME}`,
-    3
-);
-
-// add event listeners on the database connection
-// if a diconnection due to an error occurs, mongoose will automatically try to reconnect
-mongoose.connection.on("error", (err) =>
-    console.error("Database connection error:", err)
-);
-
-mongoose.connection.on("connected", () =>
-    console.log("Reconnected to the database")
-);
-
-mongoose.connection.on("disconnected", () =>
-    console.log("Disconnected from the database")
-);
-
-// create and set up the express app
-const app = express();
-app.use("/user", getUserId, userRouter);
-
-// start listening for incoming requests
-app.listen(parseInt(env.HTTP_PORT), env.HTTP_HOST, () => {
-    console.log(
-        `Auth microservice listening on '${env.HTTP_HOST}:${env.HTTP_PORT}'`
+    // connect to the database and retry up to 3 times if it fails
+    await connectToDB(
+        `mongodb://${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_NAME}`,
+        3
     );
-});
 
-export { codes };
+    // add event listeners on the database connection
+    // if a diconnection due to an error occurs, mongoose will automatically try to reconnect
+    mongoose.connection.on("error", (err) =>
+        console.error("Database connection error:", err)
+    );
+
+    mongoose.connection.on("connected", () =>
+        console.log("Reconnected to the database")
+    );
+
+    mongoose.connection.on("disconnected", () =>
+        console.log("Disconnected from the database")
+    );
+
+    // create and set up the express app
+    const app = express();
+    app.use("/user", getUserId, userRouter);
+
+    // start listening for incoming requests
+    app.listen(parseInt(env.HTTP_PORT), env.HTTP_HOST, () => {
+        console.log(
+            `Auth microservice listening on '${env.HTTP_HOST}:${env.HTTP_PORT}'`
+        );
+    });
+} catch (err) {
+    if (err instanceof Error) {
+        console.error(err.name);
+        console.error(err.message);
+    }
+    process.exit(1);
+}
+
+// http response codes
+export const codes = {
+    OK: 200,
+    CREATED: 201,
+    UNAUTHORIZED: 401,
+    INTERNAL_SERVER_ERROR: 500,
+};
--- user_management_service/src/lib/envUtils.ts
+++ user_management_service/src/lib/envUtils.ts
@@ -1,14 +1,10 @@
 // takes an object containing all environment variables (which are possibly undefined)
 // if the object has no undefined values, it is returned with no "undefined" in its type
-// if an undefined value is found, onUdefined() is called with the key of the undefined value as its argument
-function verifyEnv(
-    env: Readonly<Record<string, string | undefined>>,
-    onUndefined: (key: string) => void
-) {
-    // check for undefined environment variables
+// if an undefined value is found an error is throws
+function verifyEnv(env: Readonly<Record<string, string | undefined>>) {
     for (const [key, val] of Object.entries(env)) {
         if (val === undefined) {
-            onUndefined(key);
+            throw new Error(`Environment variable '${key}' is missing`);
         }
     }
 
