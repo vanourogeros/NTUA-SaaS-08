57eac29d2d2d8ebe3ef9399fadb058f5af3c84d4
Ioannis Protogeros <el19008@mail.ntua.gr>
2023-07-01 03:27:14 +0300

    Dockerization of fetcher

--- microservice-diagram-fetcher/index.js
+++ microservice-diagram-fetcher/index.js
@@ -7,8 +7,13 @@ app.get('/api/diagrams/:userID', async (req, res) => {
     const userID = req.params.userID;
     // TODO: Fetch diagrams for the user from all other microservices
 
-    const services = ['http://localhost:3004']; // Add all other microservices here
-    try {}
+    const services = ['http://store-line-basic:3004']; // Add all other store microservices here
+    try {
+        const requests = services.map(service => axios.get(`${service}/api/diagrams/${userID}`));
+        const responses = await Promise.all(requests);
+        const diagrams = responses.map(response => response.data);
+        return res.json({ userID, diagrams: diagrams });
+    }
     catch (err) {
         console.error(err);
         res.status(500).json({ error: 'An error occured while fetching diagrams: ' + err.message });
