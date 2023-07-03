002ec4a98024a5257b2ec7c2f7aec9db25e235fe
Georgios Alexios Kapetanakis <el19062@mail.ntua.gr>
2023-07-03 19:56:25 +0300

    Created basic_column_create_ms

---
+++ chart_creation_mss/basic_column_creation_ms/src/chart/blueprint.ts
@@ -0,0 +1,105 @@
+import type { Blueprint } from "../lib/csvUtils.js";
+
+export const blueprint: Blueprint = {
+    caption: {
+        text: {
+            type: "string",
+        },
+        align: {
+            type: "enum",
+            values: ["left", "center", "right"],
+        },
+        verticalAlign: {
+            type: "enum",
+            values: ["top", "middle", "bottom"],
+        },
+    },
+    chart: {
+        width: {
+            type: "number",
+        },
+        height: {
+            type: "number",
+        },
+        inverted: {
+            type: "boolean",
+        },
+    },
+    legend: {
+        enabled: {
+            type: "boolean",
+        },
+        align: {
+            type: "enum",
+            values: ["left", "center", "right"],
+        },
+        verticalAlign: {
+            type: "enum",
+            values: ["top", "middle", "bottom"],
+        },
+    },
+    series: [
+        {
+            name: {
+                type: "string",
+            },
+            dataLabels: {
+                enabled: {
+                    type: "boolean",
+                },
+            },
+            data: {
+                y: [
+                    {
+                        type: "number",
+                    },
+                ],
+            },
+        },
+    ],
+    subtitle: {
+        text: {
+            type: "string",
+        },
+        align: {
+            type: "enum",
+            values: ["left", "center", "right"],
+        },
+        verticalAlign: {
+            type: "enum",
+            values: ["top", "middle", "bottom"],
+        },
+    },
+    title: {
+        text: {
+            type: "string",
+        },
+        align: {
+            type: "enum",
+            values: ["left", "center", "right"],
+        },
+        verticalAlign: {
+            type: "enum",
+            values: ["top", "middle", "bottom"],
+        },
+    },
+    xAxis: {
+        title: {
+            text: {
+                type: "string",
+            },
+        },
+        categories: [
+            {
+                type: "string",
+            },
+        ],
+    },
+    yAxis: {
+        title: {
+            text: {
+                type: "string",
+            },
+        },
+    },
+};
