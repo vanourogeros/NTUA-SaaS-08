3c3823cae3c1e697d0bab2036bcf0f12015e7879
Ioannis Protogeros <el19008@mail.ntua.gr>
2023-07-06 19:34:59 +0300

    OH MY GOD I DELETED EVERYTHING

--- user_management_ms/src/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as controller from "../controllers/user.js";
-
-const router = Router();
-
-router.post("/new", controller.postNew);
-router.get("/:userId", controller.getUser);
-
-export default router;
