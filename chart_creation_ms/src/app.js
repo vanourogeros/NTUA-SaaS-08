443d06407dc5aed067c4dff4abf744953baeb22d
Ioannis Protogeros <el19008@mail.ntua.gr>
2023-07-06 04:04:13 +0300

    reverted some test changes

--- chart_creation_ms/src/app.js
+++ chart_creation_ms/src/app.js
@@ -1,5 +1,5 @@
 import env from "./env.js";
-import kafka from 'kafka-node';
+import { Kafka } from "kafkajs";
 import { createSVG } from "./lib/svgUtils.js"
 
 export const codes = {
@@ -9,85 +9,77 @@ export const codes = {
     INTERNAL_SERVER_ERROR: 500,
 };
 
-const client = new kafka.KafkaClient({ kafkaHost: 'kafka:9092' });
+const kafka = new Kafka({
+    clientId: env.KAFKA_CLIENT_ID,
+    brokers: env.KAFKA_BROKERS,
+});
 
 const producer = kafka.producer();
+producer.on("producer.connect", () => console.log("Kafka producer connected"));
+producer.on("producer.disconnect", () => console.log("Kafka producer disconnected"));
 
-const setupConsumer = () => {
+const consumer = kafka.consumer({ groupId: env.KAFKA_CONSUMER_GROUP_ID });
+consumer.on("consumer.connect", () => console.log("Kafka consumer connected"));
+consumer.on("consumer.disconnect", () => console.log("Kafka consumer disconnected"));
+
+async function startConsumer() {
     try {
-        const consumer = new kafka.Consumer(
-            client,
-            [{ topic: env.KAFKA_CONSUMER_TOPIC, partition: 0 }],
-            { autoCommit: true }
-        );
+        await consumer.connect();
+        await consumer.subscribe({ topic: env.KAFKA_CONSUMER_TOPIC });
+        console.log("subscribed to " + env.KAFKA_CONSUMER_TOPIC)
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                if (message.value == null) {
+                    console.error(`Received empty kafka message in topic '${topic}'`);
+                    return;
+                }
 
-        // Create a producer
-        const producer = new kafka.Producer(client);
-
-        producer.on('ready', function () {
-            console.log('Producer is ready');
-        });
-
-        producer.on('error', function (err) {
-            console.log('Producer is in error state');
-            console.log(err);
-        })
-
-        consumer.on('error', function (err) {
-            console.log('Error:', err);
-
-            // If the error message indicates that the topic does not exist
-            if (err.message.includes('topic')) {
-                console.log('Topic not found, retrying...');
-
-                // Delay for 5 seconds, then retry setting up the consumer
-                setTimeout(setupConsumer, 5000);
-            }
-        });
-
-        consumer.on('message', async function (message) {
-            console.log('Received message:', message.value);
-
-
-            // Create the chart
-            try {
                 const { userId, chartOptions } = JSON.parse(message.value.toString());
-                const chartSVG = await charter(chartOptions);
                 console.log("Received Message. userid: " + userId);
                 console.log("Received Message. chart_options: " + chartOptions);
                 const svgData = await createSVG(chartOptions);
                 console.log("Will now try to send SVG message at " + env.KAFKA_PRODUCER_TOPIC)
 
-                const payloads = [
-                    { topic: env.KAFKA_PRODUCER_TOPIC,
-                        messages: {
-                            value: JSON.stringify({userId, svgData}),
-                        },
-                        partition: 0 }
-                ];
-                producer.send(payloads, function (err, data) {
-                    console.log(data);
-                });
+                try {
+                    await producer.send({
+                        topic: env.KAFKA_PRODUCER_TOPIC,
+                        messages: [
+                            {
+                                value: JSON.stringify({userId, svgData}),
+                            },
+                        ],
+                    });
+                } catch (e) {
+                    console.error(
+                        `Error in producer of group [${env.KAFKA_CONSUMER_GROUP_ID}] ${e.message}`,
+                        e
+                    );
+                }
 
-            } catch (e) {
-                console.log('Error creating chart:', e);
-            }
+
+                // Acknowledge the processing of the message
+                await consumer.commitOffsets([{ topic, partition, offset: message.offset }]);
+            },
         });
-
-        consumer.on('error', function (err) {
-            console.log('Error:', err);
-        });
-
-        consumer.on('offsetOutOfRange', function (err) {
-            console.log('OffsetOutOfRange:', err);
-        });
-
     } catch (err) {
-        console.log(err);
+        console.error(
+            `Error in consumer of group [${env.KAFKA_CONSUMER_GROUP_ID}] ${e.meesage}`,
+            e
+        );
     }
 }
 
-setupConsumer();
+async function startProducer() {
+    try {
+        await producer.connect();
+        console.log("Kafka producer connected");
+    } catch (err) {
+        console.error("Error connecting Kafka producer", err);
+    }
+}
+
+startProducer();
+startConsumer();
 
 const errorTypes = ["unhandledRejection", "uncaughtException"];
 const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
